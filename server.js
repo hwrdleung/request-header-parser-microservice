7f87b8e0428fcdd8fa2b92915407aaf6c94daa99
Glitch (good-oval) <none>
2018-01-14 03:30:23 +0000

    👍🏚 Checkpoint server.js:603188/1

--- server.js
+++ server.js
@@ -12,21 +12,10 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/api/whoami", function (request, response) {
+  response.json({
+  });
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
